a90df60638171c2de2fca4e3de762cc64ace4fb0
FroZe36 <AngelofDest1ny@yandex.ru>
2022-03-11 17:16:54 +0300

    feat: add solution for tasks 1-2[HW-17]

---
+++ hw17/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//TASK 1
+
+// const a = 'true';
+// const b = false;
+// const c = 17;
+// const d = undefined;
+// const e = null;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+
+//TASK 2
+
+// let height = 15;
+// let width = 20;
+
+// if (width > height) {
+//     console.log(width);
+// } else {
+//     console.log(height);
+// }
+

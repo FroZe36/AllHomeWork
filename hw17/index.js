8728e2bcae184ef34343cf644c87d8773d4d2a6a
FroZe36 <AngelofDest1ny@yandex.ru>
2022-03-14 19:56:18 +0300

    feat: add solution for tasks 3-7 [HW-17]

--- hw17/index.js
+++ hw17/index.js
@@ -25,3 +25,64 @@
 //     console.log(height);
 // }
 
+//TASK 3
+
+// for (let i = 1; i < 20; i += 1) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+//TASK 4
+
+// let key = true;
+// let documents = true;
+// let pen = true;
+// let apple = false;
+// let orange = true;
+// let shouldGoToWork = key && documents && pen && (apple || orange);
+
+// console.log(shouldGoToWork);
+
+//TASK 5
+
+// let num = prompt('Давайте посчитаем')
+// if (num % 5 === 0 && num % 3 === 0) {
+//     alert('FizBuz');
+// } else if (num % 3 === 0) {
+//     alert('Buz');
+// } else if (num % 5 === 0) {
+//     alert('Fiz');
+// }
+
+//TASK 6
+
+// let age = prompt('Сколько вам лет?')
+// if (age >= 16 && age < 18) {
+//     alert(`Можешь выкурить сигаретку, только маме не говори`);
+// } else if (age > 18) {
+//     alert('Попей пивка');
+// } else if (age < 18) {
+//     alert("Попей колу");
+// }
+
+//TASK 7
+
+// let goTrip = prompt('Куда едем отдыхать?')
+
+// switch(goTrip) {
+//     case "юг":
+//         alert('на юг пойдешь счастье найдешь');
+//         break;
+//     case "север":
+//         alert('на север пойдешь много денег найдешь');
+//         break;
+//     case "запад":
+//         alert('на запад пойдешь верного друга найдешь');
+//         break;
+//     case "восток":
+//         alert('на восток пойдешь разработчиком станешь');
+//         break;
+//     default:
+//         alert('Попробуй испытать счастье заново');
+// }

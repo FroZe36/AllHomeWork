ae35728b28290f88068ac9cdd3d8eddf5c3a0b31
FroZe36 <AngelofDest1ny@yandex.ru>
2022-05-24 22:59:12 +0300

    fix: edit interface IMovies[HW-34]

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -6,7 +6,23 @@ interface IFaculties {
 };
 
 interface IMovies {
-    [key: string]: string | number | string[]
+    id: number,
+    title: string,
+	year: number,
+	released: string,
+	runtime: string,
+	genre: string[],
+	director: string,
+	writer: string,
+	actors: string[],
+	plot: string,
+	country: string,
+	poster: string,
+	imdbRating: number,
+	imdbVotes: number,
+	type: string,
+	boxOffice: string,
+	production: string,
 };
 
 1.
